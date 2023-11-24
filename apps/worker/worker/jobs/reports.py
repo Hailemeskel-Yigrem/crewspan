6a60242f11291105ec651320f429988c35d44999
Abel Mekonen <abelmekonen555@gmail.com>
2023-11-24 16:35:50 +0300

    feat(worker): add report export jobs

---
+++ apps/worker/worker/jobs/reports.py
@@ -0,0 +1,88 @@
+"""Report export and analytics background jobs."""
+
+from __future__ import annotations
+
+import csv
+from datetime import date
+from pathlib import Path
+
+import structlog
+from celery import shared_task
+
+from worker.base import RelayOpsTask
+from worker.config import settings
+
+logger = structlog.get_logger(__name__)
+
+
+@shared_task(base=RelayOpsTask, name="worker.jobs.reports.export_work_orders_csv")
+def export_work_orders_csv(
+    *,
+    tenant_id: str,
+    start_date: str,
+    end_date: str,
+    requested_by_id: str,
+) -> dict[str, str]:
+    """Export work orders in date range to CSV and return file path."""
+    export_dir = Path(settings.report_export_dir)
+    export_dir.mkdir(parents=True, exist_ok=True)
+    filename = f"work_orders_{tenant_id}_{start_date}_{end_date}.csv"
+    filepath = export_dir / filename
+    logger.info(
+        "reports.export.work_orders.start",
+        tenant_id=tenant_id,
+        start_date=start_date,
+        end_date=end_date,
+        requested_by_id=requested_by_id,
+    )
+    with filepath.open("w", newline="", encoding="utf-8") as fh:
+        writer = csv.writer(fh)
+        writer.writerow([
+            "order_number", "status", "priority", "customer_id",
+            "scheduled_start", "scheduled_end", "created_at",
+        ])
+        # Stub: would query work_orders for tenant and date range
+    logger.info("reports.export.work_orders.complete", filepath=str(filepath))
+    return {"filepath": str(filepath), "format": "csv"}
+
+
+@shared_task(base=RelayOpsTask, name="worker.jobs.reports.export_sla_breaches")
+def export_sla_breaches(*, tenant_id: str, month: str) -> dict[str, str]:
+    """Generate monthly SLA breach summary report."""
+    logger.info("reports.export.sla_breaches", tenant_id=tenant_id, month=month)
+    export_dir = Path(settings.report_export_dir)
+    export_dir.mkdir(parents=True, exist_ok=True)
+    filepath = export_dir / f"sla_breaches_{tenant_id}_{month}.csv"
+    with filepath.open("w", newline="", encoding="utf-8") as fh:
+        writer = csv.writer(fh)
+        writer.writerow([
+            "work_order_id", "breach_type", "expected_at",
+            "detected_at", "minutes_overdue", "escalation_level",
+        ])
+    return {"filepath": str(filepath)}
+
+
+@shared_task(base=RelayOpsTask, name="worker.jobs.reports.technician_utilization")
+def technician_utilization(
+    *,
+    tenant_id: str,
+    period_start: str,
+    period_end: str,
+) -> dict[str, object]:
+    """Compute technician utilization metrics for a reporting period."""
+    logger.info(
+        "reports.technician_utilization",
+        tenant_id=tenant_id,
+        period_start=period_start,
+        period_end=period_end,
+    )
+    metrics: list[dict[str, object]] = []
+    return {"period_start": period_start, "period_end": period_end, "technicians": metrics}
+
+
+@shared_task(base=RelayOpsTask, name="worker.jobs.reports.inventory_valuation")
+def inventory_valuation(*, tenant_id: str, as_of: str | None = None) -> dict[str, str]:
+    """Calculate total inventory valuation across all locations."""
+    valuation_date = as_of or date.today().isoformat()
+    logger.info("reports.inventory_valuation", tenant_id=tenant_id, as_of=valuation_date)
+    return {"tenant_id": tenant_id, "as_of": valuation_date, "total_value": "0.00"}
